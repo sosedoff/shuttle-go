09e50e2ba8515193c06ee17d2ec31cfca4f7b89c
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-06-19 10:36:51 -0500

    Extract helpers

--- main.go
+++ main.go
@@ -14,20 +14,6 @@ var options struct {
 	Environment string `short:"e" long:"environment" description:"Deployment environment"`
 }
 
-func terminate(message string, status int) {
-	fmt.Println(message)
-	os.Exit(1)
-}
-
-func exitWithError(err error) {
-	fmt.Println(err)
-	os.Exit(1)
-}
-
-func logStep(str string) {
-	fmt.Printf("-----> %s\n", str)
-}
-
 func main() {
 	args, err := flags.ParseArgs(&options, os.Args)
 
