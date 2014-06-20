be4ab96d0299b32d5f504c88f20cbf1902ce681b
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-06-20 17:58:31 -0500

    Remove connection helpers, move all conn specific functionality back into connections

--- connection.go
+++ connection.go
@@ -107,6 +107,44 @@ func (conn *Connection) Run(command string) string {
 	return conn.Exec(command).Output
 }
 
+func (conn *Connection) FileExists(path string) bool {
+	return conn.Exec("test -f " + path).Success
+}
+
+func (conn *Connection) DirExists(path string) bool {
+	return conn.Exec("test -d " + path).Success
+}
+
+func (conn *Connection) SymlinkExists(path string) bool {
+	return conn.Exec("test -h " + path).Success
+}
+
+func (conn *Connection) ProcessExists(pid string) bool {
+	return conn.Exec("ps -p " + pid).Success
+}
+
+func (conn *Connection) SvnInstalled() bool {
+	return conn.Exec("which svn").Success
+}
+
+func (conn *Connection) ReadFile(path string) (content string, err error) {
+	if !conn.FileExists(path) {
+		err = fmt.Errorf("File does not exist: %s", path)
+		return
+	}
+
+	result := conn.Exec("cat " + path)
+
+	if result.Success {
+		content = result.Output
+		err = nil
+	} else {
+		err = fmt.Errorf("Cant read file %s: %s", path, result.Output)
+	}
+
+	return content, err
+}
+
 func privateKeyPath() string {
 	return fmt.Sprintf("%s/.ssh/id_rsa", os.Getenv("HOME"))
 }
