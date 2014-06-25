f863001a72e64adcbae11342eb468d938d071ef6
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-06-24 21:49:49 -0500

    Implement terminal colors

--- helpers.go
+++ helpers.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/mgutz/ansi"
 	"os"
 	"strings"
 )
 
+var (
+	greenPrefix  = ansi.ColorCode("green") + "----->" + ansi.ColorCode("reset")
+	redPrefix    = ansi.ColorCode("red") + "----->" + ansi.ColorCode("reset")
+	yellowPrefix = ansi.ColorCode("yellow") + "----->" + ansi.ColorCode("reset")
+)
+
 func terminate(message string, status int) {
-	fmt.Printf("-----> ERROR: %s\n", strings.TrimSpace(message))
+	fmt.Printf("%s ERROR: %s\n", redPrefix, strings.TrimSpace(message))
 	os.Exit(1)
+}
+
+func logStep(message string) {
+	fmt.Printf("%s %s\n", greenPrefix, strings.TrimSpace(message))
 }
 
 func exitWithError(err error) {
 	fmt.Println(err)
 	os.Exit(1)
-}
-
-func logStep(str string) {
-	fmt.Printf("-----> %s\n", str)
 }
